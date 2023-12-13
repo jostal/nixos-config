01bdedf54482bf32d9e3a6219715b46c2bd444a2
Joseph Talon <josephtalon01@gmail.com>
2023-12-13 00:30:27 -0500

    CSS reloading

--- home/programs/configs/ags/js/settings/setup.js
+++ home/programs/configs/ags/js/settings/setup.js
@@ -1,6 +1,22 @@
 import App from 'resource:///com/github/Aylur/ags/app.js';
+import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
 import { globals } from './globals.js'
 
 export function init() {
   globals()
+
+  // reload css
+  Utils.monitorFile(
+    `${App.configDir}/scss`,
+
+    function() {
+      const scss = `${App.configDir}/scss/style.scss`
+      const css = `${App.configDir}/scss/style.css`
+
+      Utils.exec(`sassc ${scss} ${css}`)
+      App.resetCss()
+      App.applyCss(css)
+    },
+    'directory'
+  )
 }
