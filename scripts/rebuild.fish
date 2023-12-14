a6e5812771de740f8b046aab89e6e92eff86a9a9
Joseph Talon <josephtalon01@gmail.com>
2023-12-13 22:54:41 -0500

    fish script

--- scripts/rebuild.fish
+++ scripts/rebuild.fish
@@ -9,13 +9,18 @@ function rebuild -d "Rebuild nixos"
       set -f rebuildAgs 1
     case '-m' # commit message
       set -f commitMessage $argv[(count $argv)+1]
+  end
 
   if test $rebuildAgs -eq 1
     ags -q
   end
-
+  
   git add .
-  git commit -m $commitMessage
+  if git commit -m $commitMessage
+    echo "Commited succeeded"
+  else
+    echo "Failed to commit"
+  end
   nixos-rebuild switch
 
   if test $rebuildAgs -eq 1
