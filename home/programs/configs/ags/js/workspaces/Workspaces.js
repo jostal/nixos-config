891509d24d4b61969a21bdc1d5bf6c47380eafc6
Joseph Talon <josephtalon01@gmail.com>
2023-12-14 00:44:19 -0500

    Nixos rebuild

--- home/programs/configs/ags/js/workspaces/Workspaces.js
+++ home/programs/configs/ags/js/workspaces/Workspaces.js
@@ -1,16 +1,43 @@
 import Hyprland from 'resource:///com/github/Aylur/ags/service/hyprland.js'
 import { Widget } from "../imports.js";
 
+const getWorkspaceMonitor = (id) => {
+  let ws = Hyprland.getWorkspace(id)
+
+  switch (ws.monitor) {
+    case "DVI-D-1":
+      return "L"
+    case "HDMI-A-1":
+      return "C"
+    case "HDMI-A-2":
+      return "R"
+    default:
+      return ""
+  }
+}
+
 const WorkspaceButton = (i) => Widget.EventBox({
   className: 'wsButton',
   onPrimaryClickRelease: () => Hyprland.sendMessage(`dispatch workspace ${i}`),
-  child: Widget.Label({
-    label: `${i}`,
-    className: 'wsButtonLabel'
+  child: Widget.Box({
+    className: 'wsButtonLabel',
+    children: [
+      Widget.Label({
+        label: `${i}`,
+        className: 'wsLabel'
+      }),
+      Widget.Label({
+        label: `${getWorkspaceMonitor(i)}`,
+        className: 'wsMonitor'
+      })
+    ]
   }),
   connections: [
     [Hyprland.active.workspace, (button) => {
       button.toggleClassName('active', Hyprland.active.workspace.id === i)
+      button.toggleClassName('wsLeft', getWorkspaceMonitor(i) === 'L')
+      button.toggleClassName('wsCenter', getWorkspaceMonitor(i) === 'C')
+      button.toggleClassName('wsRight', getWorkspaceMonitor(i) === 'R')
     }]
   ]
 })
