d93255989e6260aed61fa35abd9db5623b978bf8
Joseph Talon <josephtalon01@gmail.com>
2023-12-16 00:30:58 -0500

    Nixos rebuild

---
+++ home/programs/configs/default.nix
@@ -0,0 +1,9 @@
+{ config, ... }:
+let
+  configDir = /home/jostal/nixos-config/home/programs/configs;
+  symlink = config.lib.file.mkOutOfStoreSymlink;
+in {
+  xdg.configFile = {
+    "ags".source = symlink "${configDir}/ags";
+  }
+}
