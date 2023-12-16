cd1905cd486e107544736f26fc895373768c5c2d
Joseph Talon <josephtalon01@gmail.com>
2023-12-16 00:42:59 -0500

    Nixos rebuild

--- home/programs/configs/ags/js/main.js
+++ home/programs/configs/ags/js/main.js
@@ -1,23 +1,34 @@
 import Topbar from './topbar/Topbar.js';
 import App from 'resource:///com/github/Aylur/ags/app.js';
+import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
 import { forMonitors } from './utils.js';
 import { init } from './settings/setup.js';
 import { exec } from 'resource:///com/github/Aylur/ags/utils.js';
 
+
 init();
-console.log("After init")
 
 const windows = () => [
   forMonitors(Topbar),
 ]
 
-console.log("AFTER topbar");
-
 const scss = globalThis.configDir + '/scss/style.scss';
 const css = globalThis.configDir + '/scss/style.css';
 exec(`sassc ${scss} ${css}`)
 
-console.log("After css exec")
+
+Utils.monitorFile(
+  globalThis.configDir + '/scss',
+
+  function() {
+    console.log("scss reload")
+
+    Utils.exec(`sassc ${scss} ${css}`)
+    App.resetCss()
+    App.applyCss(css)
+  },
+  'directory'
+)
 
 export default {
   windows: windows().flat(),
--- home/programs/configs/ags/js/settings/scss.js
+++ home/programs/configs/ags/js/settings/scss.js
@@ -1,20 +1,18 @@
 import App from 'resource:///com/github/Aylur/ags/app.js';
 import * as Utils from 'resource:///com/github/Aylur/ags/utils.js';
 
-export function scssWatcher() {
-  console.log('watching scss')
-  // reload css
-  Utils.monitorFile(
-    globalThis.configDir + '/scss',
+Utils.monitorFile(
+  globalThis.configDir + '/scss',
 
-    function() {
-      const scss = globalThis.configDir + '/scss/style.scss'
-      const css = globalThis.configDir + '/scss/style.css'
+  function() {
+    const scss = globalThis.configDir + '/scss/style.scss'
+    const css = globalThis.configDir + '/scss/style.css'
 
-      Utils.exec(`sassc ${scss} ${css}`)
-      App.resetCss()
-      App.applyCss(css)
-    },
-    'directory'
-  )
-}
+    console.log("scss reload")
+
+    Utils.exec(`sassc ${scss} ${css}`)
+    App.resetCss()
+    App.applyCss(css)
+  },
+  'directory'
+)
