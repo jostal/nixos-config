c5ea18cc90b060bfd67d69e26e99f6ffe3f04153
Joseph Talon <josephtalon01@gmail.com>
2023-12-16 23:52:26 -0500

    Nixos rebuild

--- home/programs/configs/nvim/lua/plugins/treesitter.lua
+++ home/programs/configs/nvim/lua/plugins/treesitter.lua
@@ -26,12 +26,12 @@ local M = {
     sync_install = true,
     ignore_install = {}, -- List of parsers to ignore installation
     refactor = {
---      highlight_definitions = {
---        enable = true,
---        -- Set to false if you have an `updatetime` of ~100.
---        clear_on_cursor_move = true,
---      },
---      highlight_current_scope = { enable = true },
+      highlight_definitions = {
+        enable = true,
+        -- Set to false if you have an `updatetime` of ~100.
+        clear_on_cursor_move = true,
+      },
+      highlight_current_scope = { enable = true },
     },
   },
   config = function(_, opts)
