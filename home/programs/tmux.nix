2d3ebb0f50a5586f0be22848147ca1b1d86ce6a3
Joseph Talon <josephtalon01@gmail.com>
2023-12-17 18:01:52 -0500

    Nixos rebuild

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -1,4 +1,38 @@
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }: 
+let
+  bg = "default";
+  fg = "default";
+  color = c: "#{@${c}}";
+
+  indicator = rec {
+    accent = color "indicator_color";
+    content = "  ";
+    module = "#[reverse,fg=${accent}]#{?client_prefix,${content},}";
+  };
+
+  current_window = rec {
+    accent = color "main_accent";
+    index = "#[reverse,fg=${accent},bg=${fg}] #I ";
+    name = "#[fg=${bg2},bg=${fg2}] #W ";
+    flags = "#{?window_flags,#{window_flags}, }";
+    module =  "${index}${name}";
+  };
+
+  window_status = rec {
+    accent = color "window_color";
+    index = "#[reverse,fg=${accent},bg=${fg}] #I ";
+    name = "#[fg=${bg2},bg=${fg2}] #W ";
+    flags = "#{?window_flags,#{window_flags}, }";
+    module =  "${index}${name}";
+  };
+
+  pwd = rec {
+    accent = color "main_accent";
+    icon = "#[fg=${accent}] ";
+    format = "#[fg=${fg}]#{b:pane_current_path}";
+    module = "${icon}${format}";
+  };
+in {
   programs.tmux = {
     enable = true;
     shell = "${pkgs.fish}/bin/fish";
@@ -33,6 +67,17 @@
       bind -n 'C-Right' if-shell "$is_vim" 'send-keys C-Right' 'select-pane -R'
       bind -n 'C-Up' if-shell "$is_vim" 'send-keys C-Up' 'select-pane -U'
       bind -n 'C-Down' if-shell "$is_vim" 'send-keys C-Down' 'select-pane -D'
+
+      # Theme
+      set-option -g @indicator_color "SteelBlue1"
+      set-option -g @main_accent "DarkOrange3"
+      set-option -g @window_color ""
+      set-option -g status-style "bg=${bg} fg=${fg}"
+      set-option -g status-left "${indicator.module}"
+      set-option -g status-right "${pwd.module}"
+      set-option -g window-status-current-format "${current_window.module}"
+      set-option -g window-status-format "${window_status.module}"
+      set-option -g window-status-seperator ""
     '';
   };
 }
