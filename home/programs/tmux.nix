bb1791eb5a5b283419101922a3c02e2a6b88e019
Joseph Talon <josephtalon01@gmail.com>
2023-12-17 16:52:17 -0500

    Nixos rebuild

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -13,8 +13,19 @@
       bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
       bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
       bind-key b set-option status
-      bind '"' split-window -v -c "#{pane_current_path}"
-      bind % split-window -h -c "#{pane_current_path}"
+
+      # Quick reload
+      bind r source-file ${pkgs.tmux}/.tmux.conf; display "reloaded"
+
+      # Split panes
+      bind C-v split-window -v -c '#{pane_current_path}'
+      bind C-h split-window -h -c '#{pane_current_path}'
+
+      # Switch panes
+      bind -n C-Left select-pane -L
+      bind -n C-Right select-pane -R
+      bind -n C-Up select-pane -U
+      bind -n C-Down select-pane -D
     '';
   };
 }
