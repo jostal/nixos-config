6d821201329329a1d6530dfbbcf0295abb43d2b8
Joseph Talon <josephtalon01@gmail.com>
2023-12-17 16:30:36 -0500

    Nixos rebuild

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -4,5 +4,17 @@
     shell = "${pkgs.fish}/bin/fish";
     terminal = "tmux-256color";
     historyLimit = 100000;
+    prefix = "C-Space";
+    escapeTime = 0;
+    keyMode = "vi";
+    extraConfig = ''
+      bind v copy-mode
+      bind -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+      bind-key b set-option status
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -h -c "#{pane_current_path}"
+    ''
   };
 }
