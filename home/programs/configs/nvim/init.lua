87f02fb963e551836330f4ab3bae30b326a53518
Joseph Talon <josephtalon01@gmail.com>
2023-12-16 23:51:20 -0500

    Nixos rebuild

--- home/programs/configs/nvim/init.lua
+++ home/programs/configs/nvim/init.lua
@@ -1,9 +1,9 @@
 require("config.options")
 require("config.lazy")
 
-vim.cmd([[let g:gruvbox_disable_terminal_colors = 0]])
-vim.cmd([[let g:gruvbox_material_transparent_background = 2]])
-vim.cmd([[colorscheme gruvbox-material]])
+-- vim.cmd([[let g:gruvbox_disable_terminal_colors = 0]])
+-- vim.cmd([[let g:gruvbox_material_transparent_background = 2]])
+-- vim.cmd([[colorscheme gruvbox-material]])
 
 vim.api.nvim_create_autocmd("User", {
   pattern = "VeryLazy",
