28a6ebf2bbdb38c6ed99d2ff608b2bd83d92d0fe
Joseph Talon <josephtalon01@gmail.com>
2023-12-18 16:07:23 -0500

    Nixos rebuild

--- home/hyprland/binds.nix
+++ home/hyprland/binds.nix
@@ -6,7 +6,7 @@ let
 	resizeActive = binding "${mainMod} CTRL" "resizeactive";
 in [
 	# Programs
-	"${mainMod}, T, exec, alacritty -e tmux attach"
+	"${mainMod}, T, exec, alacritty -e tmux attach new-window"
 	"${mainMod}, E, exec, nautilus"
 	"${mainMod}, SPACE, exec, tofi-drun --drun-launch=true"
 	
