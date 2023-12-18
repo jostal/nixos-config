f2d9b949687ed62a6c7beec1350597a9785e6b22
Joseph Talon <josephtalon01@gmail.com>
2023-12-18 02:08:10 -0500

    Nixos rebuild

--- home/programs/configs/nvim/lua/plugins/lsp/init.lua
+++ home/programs/configs/nvim/lua/plugins/lsp/init.lua
@@ -1,7 +1,50 @@
 config = function(_, opts)
-    local utils = require("utils")
-    local lspconfig = require("lspconfig")
-    local lsp_utils = require("plugins.lsp.lsp-utils")
-    lsp_utils.setup()
+  local utils = require("utils")
+  local lspconfig = require("lspconfig")
+  local lsp_utils = require("plugins.lsp.lsp-utils")
+  lsp_utils.setup()
+
+  # Setup language servers
+  lspconfig.tsserver.setup()
+  lspconfig.nil_ls.setup()
+  lspconfig.lua_ls.setup {
+    on_init = function(client)
+      local path = client.workspace_folders[1].name
+      if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+        client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+          Lua = {
+            runtime = {
+              -- Tell the language server which version of Lua you're using
+              -- (most likely LuaJIT in the case of Neovim)
+              version = 'LuaJIT'
+            },
+            -- Make the server aware of Neovim runtime files
+            workspace = {
+              checkThirdParty = false,
+              library = {
+                vim.env.VIMRUNTIME
+                -- "${3rd}/luv/library"
+                -- "${3rd}/busted/library",
+              }
+              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+              -- library = vim.api.nvim_get_runtime_file("", true)
+            }
+          }
+        })
+
+        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+      end
+      return true
+    end
+  }
+  lspconfig.rust_analyzer.setup({
+    settings = {
+      ['rust-analyzer'] = {
+        diagnostics = {
+          enable = true
+        }
+      }
+    }
+  })
 end
 config()
