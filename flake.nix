92735b2f53b43e89d1c8f0c3358f7c2793e68275
Joseph Talon <josephtalon01@gmail.com>
2024-03-22 23:19:16 -0400

    Nixos rebuild

--- flake.nix
+++ flake.nix
@@ -12,6 +12,7 @@
       "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
       "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
     ];
+    trusted-users = ["jostal" "@wheel"];
 	};
 
 	# Inputs
